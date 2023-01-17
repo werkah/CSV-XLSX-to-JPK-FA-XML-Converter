963aef92d4c5ae037456d033c42bf5cc21eb36b6
werkah <wehilaszek@student.agh.edu.pl>
2023-01-17 18:02:17 +0100

    additions

--- utils/src/main/java/pl/edu/agh/kis/pz1/util/XMLWriter.java
+++ utils/src/main/java/pl/edu/agh/kis/pz1/util/XMLWriter.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.kis.pz1.util;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
 public class XMLWriter {
 
 }
---
+++ utils/src/test/java/CSVReaderTest.java
@@ -0,0 +1,2 @@
+public class CSVReaderTest {
+}
